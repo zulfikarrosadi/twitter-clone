980c3f0e592e22cb66380e1a07f1add23378d1f2
Zulfikar31 <zulfikarrosadi31@gmail.com>
2021-10-06 10:48:05 +0700

    change function's name
    from tweet_create_get to getTweetPage
    from tweet_create_post to addTweet

--- routes/tweetRoutes.js
+++ routes/tweetRoutes.js
@@ -1,13 +1,10 @@
 const { Router } = require('express');
-const {
-  tweet_create_get,
-  tweet_create_post,
-} = require('../controllers/tweetControllers');
+const { getTweetPage, addTweet } = require('../controllers/tweetControllers');
 
 const router = Router();
 
 router.get('/', (req, res) => res.send('work'));
-router.get('/create-tweet', tweet_create_get);
-router.post('/create-tweet', tweet_create_post);
+router.get('/create-tweet', getTweetPage);
+router.post('/create-tweet', addTweet);
 
 module.exports = router;
