b6e4f39c5e3f85467bab7d7bb32e8b597fe93090
Zulfikar31 <zulfikarrosadi31@gmail.com>
2021-10-08 15:36:02 +0700

    add routing to get tweets
    /tweets                   get the latest 10 tweets
    /tweets/infinite/:cursor  get the next 10 tweets based on last id from /tweets (cursor)

--- routes/tweetRoutes.js
+++ routes/tweetRoutes.js
@@ -1,10 +1,17 @@
 const { Router } = require('express');
-const { getTweetPage, addTweet } = require('../controllers/tweetControllers');
+const {
+  getTweetPage,
+  addTweet,
+  getTweets,
+  getInfiniteTweets,
+} = require('../controllers/tweetControllers');
 
 const router = Router();
 
 router.get('/', (req, res) => res.send('work'));
 router.get('/create-tweet', getTweetPage);
 router.post('/create-tweet', addTweet);
+router.get('/tweets', getTweets);
+router.get('/tweets/infinite/:cursor', getInfiniteTweets);
 
 module.exports = router;
