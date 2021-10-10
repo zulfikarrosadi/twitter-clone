52dce96e7b6b5fd13b7c7ee5788fedbddb67ee1d
Zulfikar31 <zulfikarrosadi31@gmail.com>
2021-10-10 20:12:42 +0700

    delete routes for getting create tweet page

--- routes/tweetRoutes.js
+++ routes/tweetRoutes.js
@@ -1,6 +1,5 @@
 const { Router } = require('express');
 const {
-  getTweetPage,
   addTweet,
   getTweets,
   getInfiniteTweets,
@@ -14,7 +13,6 @@ router.get('/', (req, res) => res.send('work'));
 router.get('/tweets', getTweets);
 router.get('/tweets/infinite/:cursor', getInfiniteTweets);
 router.get('/tweet/:id', getSingleTweet);
-router.get('/create-tweet', getTweetPage);
 
 router.post('/create-tweet', addTweet);
 router.delete('/tweet', deleteTweet);
