ad98ffb8f718e9ca1d1b20b2c6ca10ecc6868495
Zulfikar31 <zulfikarrosadi31@gmail.com>
2021-10-11 19:54:18 +0700

    fixed error when getting env variable

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-const PORT = 3000 || env.proccess.PORT;
+const PORT = 3000 || process.env.PORT;
 app.listen(PORT, () => console.log(`listening to http://localhost:${PORT}`));
 
 app.use(tweetRoutes);
