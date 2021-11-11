82b1cae2a8077cb11da23a8b664216ebe6ad641e
Zulfikar31 <zulfikarrosadi31@gmail.com>
2021-11-11 14:15:08 +0700

    used /tweets to create consistency endpoint for tweet operation

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
+app.use('/tweets', tweetRoutes);
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`listening to http://localhost:${PORT}`));
-
-app.use(tweetRoutes);
