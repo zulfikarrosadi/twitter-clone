d3a40e45ca5356295cb04666ed5a8fa6d3eb3a8b
zulfikarrosadi <zulfikarrosadi31@gmail.com>
2022-01-26 12:14:32 +0700

    delete unused trycatch block

--- services/tweetService.js
+++ services/tweetService.js
@@ -3,94 +3,59 @@ const { PrismaClient, Prisma } = require('@prisma/client');
 const prisma = new PrismaClient();
 
 const createTweet = async (options) => {
-  try {
-    await prisma.tweet_parent.create({ data: options });
-    return true;
-  } catch (error) {
-    console.log(error);
-    return false;
-  }
+  const result = await prisma.tweet_parent.create({ data: options });
+  return result;
 };
 
 const getSingleTweetById = async (id) => {
-  try {
-    const tweet = await prisma.tweet_parent.findUnique({
-      where: { id },
-      include: {
-        tweet_child: {
-          select: { id: true, tweet: true },
-        },
-        tweet_photos: {
-          select: { images: true },
-        },
+  const result = await prisma.tweet_parent.findUnique({
+    where: { id },
+    include: {
+      tweet_child: {
+        select: { id: true, tweet: true },
       },
-    });
-    return tweet;
-  } catch (error) {
-    console.log(error);
-    return false;
-  }
+      tweet_photos: {
+        select: { images: true },
+      },
+    },
+  });
+  return result;
 };
 
 const getAllTweets = async (options) => {
-  try {
-    const tweets = await prisma.tweet_parent.findMany(options);
-    return tweets;
-  } catch (error) {
-    console.log(error);
-    return [];
-  }
+  const result = await prisma.tweet_parent.findMany(options);
+  return result;
 };
 
 const updateTweetById = async (data) => {
-  try {
-    await prisma.tweet_parent.update(data);
-    return true;
-  } catch (error) {
-    console.log(error);
-    return false;
-  }
+  const result = await prisma.tweet_parent.update(data);
+  return result;
 };
 
 const getPhotofilename = async (id) => {
-  try {
-    const photoFileNames = await prisma.$queryRaw(
-      Prisma.sql`SELECT tp.images AS tweet_photos FROM tweet_parent AS tps
-      JOIN tweet_photos AS tp ON tps.id = tp.id_tweet_parent WHERE tps.id = ${id}`,
-    );
-    return photoFileNames;
-  } catch (error) {
-    console.log(error);
-    return [];
-  }
+  const result = await prisma.$queryRaw(
+    Prisma.sql`SELECT tp.images AS tweet_photos FROM tweet_parent AS tps
+    JOIN tweet_photos AS tp ON tps.id = tp.id_tweet_parent WHERE tps.id = ${id}`,
+  );
+  return result;
 };
 
 const deleteRelatedTweetChildAndTweetPhotos = async (id) => {
-  try {
-    await prisma.tweet_parent.update({
-      where: { id },
-      data: {
-        tweet_child: { deleteMany: { id_tweet_parent: id } },
-        tweet_photos: { deleteMany: { id_tweet_parent: id } },
-      },
-    });
-    return true;
-  } catch (error) {
-    console.log(error);
-    return false;
-  }
+  const result = await prisma.tweet_parent.update({
+    where: { id },
+    data: {
+      tweet_child: { deleteMany: { id_tweet_parent: id } },
+      tweet_photos: { deleteMany: { id_tweet_parent: id } },
+    },
+  });
+  return result;
 };
 
 const deleteTweetParentById = async (id) => {
-  try {
-    await prisma.tweet_parent.delete({
-      where: { id },
-    });
-    return true;
-  } catch (error) {
-    console.log(error);
-    return false;
-  }
+  const result = await prisma.tweet_parent.delete({
+    where: { id },
+  });
+  return result;
 };
 
 module.exports = {
