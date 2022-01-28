798368696256b488e91e9558b4921ce16959cbed
zulfikarrosadi <zulfikarrosadi31@gmail.com>
2022-01-28 07:44:25 +0700

    added routes to get comment resources

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 
 const tweetRoutes = require('./routes/tweetRoutes');
+const commentRoutes = require('./routes/commentRoute');
 
 const app = express();
 
@@ -11,6 +12,7 @@ app.use(express.json());
 app.use(express.static('public'));
 
 app.use('/tweets', tweetRoutes);
+app.use('/tweets/comments', commentRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`listening to http://localhost:${PORT}`));
