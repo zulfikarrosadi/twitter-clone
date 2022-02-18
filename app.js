15ce51a5f7c3b4c7ccb58eba7aa45e232f06b9dc
zulfikarrosadi <zulfikarrosadi31@gmail.com>
2022-02-19 00:09:51 +0700

    added and registered cookie-parser

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
 const cors = require('cors');
 
 const tweetRoutes = require('./routes/tweetRoute');
@@ -11,6 +12,7 @@ app.use(cors({ origin: '*' }));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
+app.use(cookieParser());
 
 app.use('/tweets', tweetRoutes);
 app.use('/tweets/comments', commentRoutes);
