a5789f5325997a57684a13813c04a3fedba04390
zulfikarrosadi <zulfikarrosadi31@gmail.com>
2022-02-28 08:06:36 +0700

    added logout user handler

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,16 +1,10 @@
-const { createUser } = require('../services/userService');
+const { createUser, getUser } = require('../services/userService');
 const { uniqueConstraintErrorHandler } = require('../utils/userErrorHandler');
 const hashPassword = require('../utils/hashPassword');
 const getTimelapse = require('../utils/getTimelapse');
-
-// TODO
-// - error handling for unique constraint
-// - make error response schema and more reuesable
-// - express validator
-// - hash password
-// - utils to get timelapse (considering using prisma.$use middleware to use logging)
-// - do some research about the best response schema from rest api
-// - how to know uesr ip js
+const verifyPassword = require('../utils/verifyPassword');
+const createHashUserId = require('../utils/hashUserId');
+const config = require('../constant/config');
 
 const addUser = async (req, res) => {
   const beforeTime = new Date().getTime();
@@ -38,4 +32,36 @@ const addUser = async (req, res) => {
   }
 };
 
-module.exports = { addUser };
+const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await getUser(email);
+    if (!user) throw new Error('No user email');
+
+    const isVerified = await verifyPassword(password, user.password);
+    if (!isVerified) throw new Error('Wrong password');
+
+    const hashedUserId = createHashUserId(user.id);
+
+    // store username and hashedUserId(as a key) in redis
+
+    return res
+      .status(200)
+      .cookie('JERAWAT', hashedUserId, {
+        maxAge: config.ONE_HOUR,
+        SameSite: 'Lax',
+        httpOnly: true,
+      })
+      .json({ message: 'ok' });
+  } catch (error) {
+    return res.status(400).json({ message: error.message });
+  }
+};
+
+// eslint-disable-next-line arrow-body-style
+const logOutUser = async (req, res) => {
+  // remove user information on redis
+  return res.status(200).cookie('JERAWAT', '', { maxAge: 1 }).send('Log out');
+};
+
+module.exports = { addUser, loginUser, logOutUser };
