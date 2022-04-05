e43907138e7f2b00bfbe5ae006f2adc0010eef21
Zulfikar31 <zulfikarrosadi31@gmail.com>
2022-04-05 16:45:09 +0700

    canged variable name from one_hour to exp_time

--- constant/config.js
+++ constant/config.js
@@ -1,5 +1,5 @@
 // 1000 = 1 second in milisecond
 // 3600 = 1 hour in second
-const ONE_HOUR = 1000 * 3600;
+const EXP_TIME = 1000 * 3600;
 
-module.exports = { ONE_HOUR };
+module.exports = { EXP_TIME };
