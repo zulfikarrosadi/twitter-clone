da4aba77dde80339258f6f705399f4dfbfa8d95e
Zulfikar31 <zulfikarrosadi31@gmail.com>
2022-04-24 18:26:42 +0700

    added new max_file_item constant

--- src/constant/config.js
+++ src/constant/config.js
@@ -1,5 +1,6 @@
 // 1000 = 1 second in milisecond
 // 3600 = 1 hour in second
 const EXP_TIME = 1000 * 3600;
+const MAX_FILE_ITEM = 10;
 
-module.exports = { EXP_TIME };
+module.exports = { EXP_TIME, MAX_FILE_ITEM };
