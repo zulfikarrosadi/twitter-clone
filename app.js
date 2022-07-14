30497fce8fc195d7528d8e8666dacd203b2f4fe9
Zulfikar Rosadi <zulfikarrosadi31@gmail.com>
2022-07-14 15:00:34 +0700

    added exoress-formidable to parse form-data

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
+const formData = require('express-form-data');
 
 const tweetRoutes = require('./src/routes/tweetRoute');
 const commentRoutes = require('./src/routes/commentRoute');
@@ -13,6 +14,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 app.use(cookieParser());
+app.use(formData.parse());
 
 app.use('/tweets', tweetRoutes);
 app.use('/tweets/comments', commentRoutes);
