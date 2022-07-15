01eb90b01a09c4b003226c3d729fc9007351a31c
Zulfikar Rosadi <zulfikarrosadi31@gmail.com>
2022-07-15 17:53:51 +0700

    add deserializeUser function to verify user session

---
+++ src/middlewares/deserializeUser.js
@@ -0,0 +1,39 @@
+const { getSession } = require('../services/redisService');
+const { createToken, verifyJwt } = require('../utils/jwtUtil');
+const { generateSessionId } = require('../utils/sessionUtil');
+
+const deserializeUser = async (req, res, next) => {
+  const { accessToken, refreshToken } = req.cookies;
+
+  // check integrity of access token
+
+  if (!accessToken) return next();
+  const { payload: accessPayload } = verifyJwt(accessToken);
+
+  if (accessPayload) {
+    req.user = accessPayload;
+    return next();
+  }
+
+  // if access token is invalid, then check the integrity of refresh token
+
+  if (!refreshToken) return next();
+  const { payload: refreshPayload } = verifyJwt(refreshToken);
+
+  if (!refreshPayload) return next();
+  const isRefreshTokenValid = await getSession(refreshPayload.refreshSessionId);
+
+  if (!isRefreshTokenValid) return next();
+  const { accessToken: newAccessToken } = createToken(
+    refreshPayload.userSettingId,
+    refreshPayload.userProfileId,
+    { accessSessionId: generateSessionId().accessSessionId },
+  );
+
+  res.cookie('accessToken', newAccessToken, { httpOnly: true });
+  req.user = verifyJwt(newAccessToken).payload;
+
+  return next();
+};
+
+module.exports = { deserializeUser };
