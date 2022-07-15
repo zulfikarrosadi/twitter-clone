ba1ca4ad26ff3f2b983d4dc0fc0a9274bde5343f
Zulfikar Rosadi <zulfikarrosadi31@gmail.com>
2022-07-15 09:50:01 +0700

    moved authenctication route to independent route

---
+++ src/routes/authRoute.js
@@ -0,0 +1,19 @@
+const { Router } = require('express');
+const {
+  userValidationSchema,
+  userLoginValidationSchema,
+} = require('../constant/userValidationSchema');
+const {
+  addUser,
+  loginUser,
+  logOutUser,
+} = require('../controllers/userController');
+const validateUser = require('../middlewares/validateUser');
+
+const router = Router();
+
+router.post('/register', validateUser(userValidationSchema), addUser);
+router.post('/login', validateUser(userLoginValidationSchema), loginUser);
+router.post('/logout', logOutUser);
+
+module.exports = router;
--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -1,28 +1,16 @@
 const { Router } = require('express');
 const {
-  userValidationSchema,
-  userLoginValidationSchema,
-} = require('../constant/userValidationSchema');
-const {
-  addUser,
-  loginUser,
-  logOutUser,
   getUserSettings,
   updateUserSettings,
   updateUserProfile,
   updateUserPassword,
 } = require('../controllers/userController');
 const hasUserLogin = require('../middlewares/hasUserLogin');
-const validateUser = require('../middlewares/validateUser');
 
 const router = Router();
 
-router.post('/register', validateUser(userValidationSchema), addUser);
-router.post('/login', validateUser(userLoginValidationSchema), loginUser);
-
 router.use(hasUserLogin);
 
-router.post('/logout', logOutUser);
 router.get('/settings', getUserSettings);
 router.patch('/settings/information', updateUserSettings);
 router.patch('/settings/password', updateUserPassword);
