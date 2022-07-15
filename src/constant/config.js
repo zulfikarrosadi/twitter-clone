3a514f759635d81999d997cc89de8235196c2cce
Zulfikar Rosadi <zulfikarrosadi31@gmail.com>
2022-07-15 18:01:19 +0700

    importing private and public key to config

--- src/constant/config.js
+++ src/constant/config.js
@@ -1,5 +1,9 @@
+require('dotenv').config();
+
 module.exports = {
   COOKIE_EXP_TIME: 1000 * 3600, // 1 hour in milisecond
   MAX_FILE_ITEM: 10,
   REDIS_URL: process.env.REDIS_URL,
+  PRIVATE_KEY: `${process.env.PRIVATE_KEY}`.replace(/\\n/gm, '\n'),
+  PUBLIC_KEY: `${process.env.PUBLIC_KEY}`.replace(/\\n/gm, '\n'),
 };
