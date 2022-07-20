17b67373651b7d48f02d84e5f9d60bb0b580725f
Zulfikar Rosadi <zulfikarrosadi31@gmail.com>
2022-07-20 13:45:11 +0700

    removed express.json and express.urlencoded middleware because we only receive the data from formData

--- app.js
+++ app.js
@@ -10,8 +10,6 @@ const { deserializeUser } = require('./src/middlewares/deserializeUser');
 const app = express();
 
 app.use(cors({ origin: '*' }));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use(cookieParser());
 
