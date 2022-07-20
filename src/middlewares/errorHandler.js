bb5fddeae0cffbf54160d75160d8c2b12e6d8bee
Zulfikar Rosadi <zulfikarrosadi31@gmail.com>
2022-07-20 13:54:00 +0700

    delete errorHandler

--- src/middlewares/errorHandler.js
+++
@@ -1,6 +0,0 @@
-// eslint-disable-next-line arrow-body-style
-const errorHanlder = (err, req, res, next) => {
-  res.status(400).json({ message: err.message });
-  next();
-};
-module.exports = errorHanlder;
